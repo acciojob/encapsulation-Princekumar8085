e806ebe75ecdaf17c638d952ab86db8f1c01ba54
prince kumar <bablu875727@gmail.com>
2023-01-13 22:05:43 +0530

    solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+
+        RWOnly obj = new RWOnly();
+        obj.setName("ram");
+        obj.setAge(20);
+        System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+    }
 }
